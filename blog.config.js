4a8492feb5f9930a62fcdff1ee27e34edfd6119d
Cicero010 <117760132+Cicero010@users.noreply.github.com>
2024-12-24 01:41:39 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -28,7 +28,8 @@ const BLOG = {
   BIO: process.env.NEXT_PUBLIC_BIO || '你好，我是知非', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://ynwws.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '知非的博客', // 网站关键词 英文逗号隔开
-
+  SINCE: process.env.NEXT_SINCE || 2024, // 建站年份
+  
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
